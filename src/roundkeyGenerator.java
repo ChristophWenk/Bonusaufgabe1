deb3a78ce2a369bf500c41a78b5b5d6b139a11b9
kassia <celine.albrecher@students.fhnw.ch>
2019-04-16 16:59:34 +0200

    Stand nach Besprechung mit Christoph

--- src/roundkeyGenerator.java
+++ src/roundkeyGenerator.java
@@ -1,37 +1,46 @@
 import java.sql.Array;
+import java.util.ArrayList;
 
 public class roundkeyGenerator {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        //https://www.java-forum.org/thema/wie-eine-int-zahl-in-die-einzelnen-ziffern-zerlegen.23009/
-        long k = 0b00111010100101001101011000111111;
-        String str = Long.toString(k);
-        long[] ia = new long[str.length()];
-        int r = 2;
+        //Runden angeben
+        int r =4;
+        int m= 4;
 
-        int j = 0;
-        while (j<r) {
-            for (int i = 0; i < ia.length; i++) {
-                i= i-1;
-                int n = 0;
-                while (n >15){
-                    long[] key = new long[str.length()];
-                    key = ia;
+        int bitLength = r*m;
+        // Gesamtschluessel "totalKey" instanzieren und in einem Array abfuellen
+        String totalKey = "00111010100101001101011000111111";
+        int totalKeyLength = totalKey.length();
+        char[] totalKeyArray = new char[totalKeyLength];
 
-                    n++;
+        ArrayList<Character> roundkeys = new ArrayList<Character>();
+        char[] roundkeyArray = new char[bitLength];
 
-                    System.out.println(k);
+            for (int i = 0; i < totalKeyArray.length; i++) {
+                totalKeyArray[i] = totalKey.charAt(i);
+            }
+
+            int i= 0;
+            // Runden zaehlen
+            while(i<r){
+                // Rundenschluessel abfuellen
+                for (int j=0; j<bitLength; j++){ ;
+                    // Einzelner Schlüssel erstellen
+                        roundkeyArray[j] = totalKeyArray[(i*r)+j];
+                        System.out.print(roundkeyArray[j]);
+                    }
+                i++;
                 }
 
+            String key = String.copyValueOf(roundkeyArray);
+            String[] roundkeys2 = key.split("(?<=\\G\\d{16})");
+            System.out.println(roundkeys2[0]);
 
-            }
-        }
-        System.out.println(Long.toBinaryString(k));
+
 
     }
 
-
-
-
 }
+
