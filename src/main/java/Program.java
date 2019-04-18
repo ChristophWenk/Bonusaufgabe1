ca2c002a14613d9711a4d6180213e9ccf89123cc
ChristophWenk <christoph.wenk@students.fhnw.ch>
2019-04-18 22:31:35 +0200

    Some more refactoring

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,9 +2,8 @@ public class Program {
     public static void main(String[] args) {
         SPN spn = new SPN(4,4,4,32);
         Tools tools = new Tools();
-        tools.readChiffre("chiffre.txt");
+        tools.readChiffre("src/main/resources/chiffre.txt");
         //spn.dechiffre(file);
-
 
     }
 }
